9d93fade5db82d25f33eb31f8f6656411b31b65d
Василюк Матвей 350501 <vasilukmat23@gmail.com>
2026-06-24 20:56:33 +0300

    init

---
+++ src/main/java/com/matvey/objectaccountingservice/ObjectAccountingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.matvey.objectaccountingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjectAccountingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjectAccountingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/matvey/objectaccountingservice/ObjectAccountingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matvey.objectaccountingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjectAccountingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
